4405d5fac76dd761acf57e4cb6a8ff2296c48d6a
Joyful Lemabari <lemabarijoyful@gmail.com>
2026-07-31 12:50:12 +0100

    Debug SMTP

--- ypsg-backend/config/mailer.js
+++ ypsg-backend/config/mailer.js
@@ -1,24 +1,33 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: process.env.SMTP_SECURE === "true",
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
-    },
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: process.env.SMTP_SECURE === 'true',
 
-    connectionTimeout: 60000,
-    greetingTimeout: 60000,
-    socketTimeout: 60000
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+
+  logger: true,
+  debug: true
 });
 
 async function verifyMailer() {
-    await transporter.verify();
+  console.log("SMTP CONFIG:");
+  console.log({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE,
+    user: process.env.SMTP_USER
+  });
+
+  await transporter.verify();
 }
 
-module.exports = {
-    transporter,
-    verifyMailer
-};
+module.exports = { transporter, verifyMailer };
